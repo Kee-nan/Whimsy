262813704d44c05ff53d7d4b0f1e6016ed1f5122
Keenan Herbe <kherbe81@hotmail.com>
2024-06-30 01:02:34 -0400

    Added text to main media cards

--- frontend/src/pages/main.js
+++ frontend/src/pages/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 import AppNavbar from '../components/Navbar';
 
 const MainPage = () => {
@@ -26,43 +26,64 @@ const MainPage = () => {
         </Container>
       </div>
 
-      <div className="features-section bg-white py-5" style={{ minHeight: 'calc(100vh - 80px)', paddingBottom: '80px' }}>
+      <div className="features-section bg-white py-5" style={{ paddingBottom: '150px' }}>
         <Container>
           <Row className="justify-content-center">
             <Col md={8} className="text-center">
               <Card className="animate__animated animate__zoomIn mb-3" style={{ width: '100%' }}>
                 <Card.Body>
-                  <Card.Title>Title</Card.Title>
-                  <Card.Text>Bio ailebfgwsairlbgailjsdrbgrldjbfgliawsrbgpoagbpoa;gbrfpgjbarpogbedrg</Card.Text>
-                  <Button variant="primary">Learn More</Button>
+                  <Card.Title>Music and Albums</Card.Title>
+                  <Card.Text>
+                    Music has been an integral part of human culture for thousands of years, evolving from ancient tribal rhythms and folk songs to the complex and diverse genres we enjoy today. The concept of the album as a collection of songs organized around a central theme emerged in the mid-20th century, transforming how artists present their work and how listeners experience music. From vinyl records to digital streams, the format of music consumption has continually evolved. With Spotify, you can access an extensive library of music that spans genres, eras, and cultures, offering a rich exploration of the art form. Spotify's powerful algorithms also help discover new favorites and dive deep into the rich history of music.
+                  </Card.Text>
                 </Card.Body>
               </Card>
               <Card className="animate__animated animate__zoomIn mb-3" style={{ width: '100%' }}>
                 <Card.Body>
-                  <Card.Title>Title</Card.Title>
-                  <Card.Text>Bio ailebfgwsairlbgailjsdrbgrldjbfgliawsrbgpoagbpoa;gbrfpgjbarpogbedrg</Card.Text>
-                  <Button variant="primary">Learn More</Button>
+                  <Card.Title>Cinema</Card.Title>
+                  <Card.Text>
+                    Cinema, as an art form and entertainment medium, has undergone remarkable evolution since its inception in the late 19th century. From the silent films of early cinema to the advent of sound and color, and now to the digital and streaming revolutions, movies have continuously adapted to new technologies and storytelling techniques. Cinema has become a global language, reflecting and shaping cultures through its narrative and visual artistry. Platforms like TMDB offer a comprehensive resource for discovering and exploring the vast array of films, from classic masterpieces to contemporary blockbusters. TMDB provides insights into movie details, reviews, and more, enriching your cinematic experience.
+                  </Card.Text>
                 </Card.Body>
               </Card>
               <Card className="animate__animated animate__zoomIn mb-3" style={{ width: '100%' }}>
                 <Card.Body>
-                  <Card.Title>Title</Card.Title>
-                  <Card.Text>Bio ailebfgwsairlbgailjsdrbgrldjbfgliawsrbgpoagbpoa;gbrfpgjbarpogbedrg</Card.Text>
-                  <Button variant="primary">Learn More</Button>
+                  <Card.Title>Television Series</Card.Title>
+                  <Card.Text>
+                    Television series have revolutionized the way we consume stories and entertainment. Since the early days of TV, serialized storytelling has allowed for deeper character development and complex narratives that unfold over time. From the golden age of TV dramas and sitcoms to the streaming era’s original series, TV shows have become a central part of modern entertainment. They offer a mirror to our society, exploring various themes and issues in engaging ways. TVMaze provides an extensive database for exploring TV series, including detailed information on episodes, cast, and show history, helping viewers discover and appreciate the breadth of television content available.
+                  </Card.Text>
                 </Card.Body>
               </Card>
               <Card className="animate__animated animate__zoomIn mb-3" style={{ width: '100%' }}>
                 <Card.Body>
-                  <Card.Title>Title</Card.Title>
-                  <Card.Text>Bio ailebfgwsairlbgailjsdrbgrldjbfgliawsrbgpoagbpoa;gbrfpgjbarpogbedrg</Card.Text>
-                  <Button variant="primary">Learn More</Button>
+                  <Card.Title>Literature</Card.Title>
+                  <Card.Text>
+                    Literature has been a cornerstone of human expression, capturing the essence of our thoughts, dreams, and cultural heritage through written words. From ancient epic poems and classical texts to modern novels and poetry, literature serves as a record of human experience and imagination. It explores a wide range of genres and themes, reflecting both individual and collective stories. The advent of printing technology democratized access to books, making literature more widely available. With Google Books, readers can explore an extensive collection of literary works, dive into historical texts, and discover new and classic literature from around the world.
+                  </Card.Text>
                 </Card.Body>
               </Card>
               <Card className="animate__animated animate__zoomIn mb-3" style={{ width: '100%' }}>
                 <Card.Body>
-                  <Card.Title>Title</Card.Title>
-                  <Card.Text>Bio ailebfgwsairlbgailjsdrbgrldjbfgliawsrbgpoagbpoa;gbrfpgjbarpogbedrg</Card.Text>
-                  <Button variant="primary">Learn More</Button>
+                  <Card.Title>Video Games</Card.Title>
+                  <Card.Text>
+                    Video games represent a unique fusion of art, technology, and interactive entertainment. Since the early days of arcade games and 8-bit consoles, video games have evolved into immersive experiences that blend storytelling, visuals, and gameplay. The medium has grown into a multi-billion-dollar industry with diverse genres and platforms, from casual mobile games to complex open-world adventures. Video games offer a way to engage with creative content actively and have become a significant cultural phenomenon. Explore the vibrant world of gaming through various platforms, and appreciate the creativity and technical skill that goes into developing these interactive experiences.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+              <Card className="animate__animated animate__zoomIn mb-3" style={{ width: '100%' }}>
+                <Card.Body>
+                  <Card.Title>Anime</Card.Title>
+                  <Card.Text>
+                    Anime, a style of animation that originated in Japan, has become a global cultural phenomenon known for its vibrant art, fantastical themes, and deep storytelling. Emerging in the early 20th century, anime has evolved through various art movements and technological advancements, gaining international recognition and influence. Today, it encompasses a wide range of genres and styles, captivating audiences worldwide with its unique approach to animation and narrative. Jikan MyAnimeList offers a comprehensive platform to explore and discover anime series and movies, providing detailed information on titles, episodes, and character profiles.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+              <Card className="animate__animated animate__zoomIn mb-3" style={{ width: '100%' }}>
+                <Card.Body>
+                  <Card.Title>Manga</Card.Title>
+                  <Card.Text>
+                    Manga, the Japanese art of comic book and graphic novel storytelling, has a rich history that traces back to ancient Japanese art forms. In the modern era, manga has grown into a diverse and influential medium that spans various genres and demographics. Known for its distinctive art style and serialized storytelling, manga has had a significant impact on global pop culture. It provides an immersive reading experience with detailed illustrations and compelling narratives. Jikan MyAnimeList offers an extensive database for manga, allowing readers to discover and explore a wide range of titles and series, enhancing their appreciation of this influential medium.
+                  </Card.Text>
                 </Card.Body>
               </Card>
             </Col>
@@ -70,7 +91,7 @@ const MainPage = () => {
         </Container>
       </div>
 
-      <footer className="bg-dark text-light py-3" style={{ position: 'fixed', bottom: 0, width: '100%' }}>
+      <footer className="bg-dark text-light py-3">
         <Container>
           <Row className="justify-content-center">
             <Col md={6} className="text-center">
@@ -85,6 +106,7 @@ const MainPage = () => {
 };
 
 export default MainPage;
+
 
 
 
