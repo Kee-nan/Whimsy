d82393717bf9e6ebf71878130531a3edfcb0d4ed
Keenan Herbe <kherbe81@hotmail.com>
2025-05-03 15:42:18 -0400

    spotify connect button removal

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Container, Button, Card } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import AppNavbar from '../components/Navbar';
 import AccountDetailsCard from '../components/AccountDetailsCard';
 import UserProfileCard from '../components/ProfileCard';
 
 const Profile = () => {
-  const [spotifyToken, setSpotifyToken] = useState('');
-  const [spotifyConnected, setSpotifyConnected] = useState(false);
   const [user, setUser] = useState({
     firstName: '',
     lastName: '',
@@ -18,26 +16,6 @@ const Profile = () => {
   });
 
   useEffect(() => {
-    const fetchToken = async () => {
-      const code = new URLSearchParams(window.location.search).get('code');
-      if (code) {
-        try {
-          const { data } = await axios.post('http://localhost:5000/auth/spotify/callback', { code });
-          localStorage.setItem('spotifyToken', data.access_token);
-          setSpotifyToken(data.access_token);
-          setSpotifyConnected(true);
-          window.history.replaceState({}, document.title, '/profile');
-        } catch (error) {
-          console.error(error);
-        }
-      } else {
-        const token = localStorage.getItem('spotifyToken');
-        if (token) {
-          setSpotifyToken(token);
-          setSpotifyConnected(true);
-        }
-      }
-    };
 
     //Get User Details from Database to use to Load on page
     const fetchUserDetails = async () => {
@@ -60,7 +38,6 @@ const Profile = () => {
       }
     };
 
-    fetchToken();
     fetchUserDetails();
   }, []);
 
@@ -74,14 +51,6 @@ const Profile = () => {
         <UserProfileCard user={user} setUser={setUser} />
 
         <AccountDetailsCard user={user} />
-
-        <Card className="profile-card account-details-card mb-4">
-          <Card.Body>
-            <Card.Title>Spotify</Card.Title>
-            <Button className='primaryButton' href="http://localhost:5000/auth/spotify">Connect to Spotify</Button>
-            <p>Status: {spotifyConnected ? <span style={{ color: 'green' }}>Connected</span> : <span style={{ color: 'darkred' }}>Not Connected</span>}</p>
-          </Card.Body>
-        </Card>
 
       </Container>
     </>
